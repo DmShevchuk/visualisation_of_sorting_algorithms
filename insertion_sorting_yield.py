29f9aa726dc5646d0c362fe8d0b8f5a1e74ae73f
Dmitrii-Shevchuk <dmitriishevchuk2003@yandex.ru>
2020-05-19 21:45:48 +0300

    Added insertion sorting with generator

---
+++ insertion_sorting_yield.py
@@ -0,0 +1,61 @@
+import pygame
+import os
+from time import sleep
+from random import randint, shuffle
+
+os.environ['SDL_VIDEO_CENTERED'] = '0'
+
+pygame.init()
+SIZE = (602, 650)
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption('Insertion sorting')
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+screen.fill(BLACK)
+
+list_for_sorting = list(range(1, 601))
+
+for _ in range(randint(1, 11)):
+    shuffle(list_for_sorting)
+
+
+def insertion_sorting(lst):
+    for i in range(1, len(lst)):
+        item_to_insert = lst[i]
+        j = i - 1
+        while j >= 0 and lst[j] > item_to_insert:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = item_to_insert
+        yield lst
+
+
+list_ = insertion_sorting(list_for_sorting)
+
+running = True
+time_delta = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(BLACK)
+
+    try:
+        current_list = next(list_)
+
+        for k, value in enumerate(current_list):
+            x, y = k, SIZE[1] - value
+            width, height = 2, value
+            color = WHITE
+
+            pygame.draw.rect(screen, color, (x, y, width, height))
+
+        pygame.display.flip()
+        sleep(time_delta)
+
+    except StopIteration:
+        pass
